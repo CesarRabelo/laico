c7ffd7a34cb823e28815de286ac5612878993280
César Rabelo <cesarrabelo@gmail.com>
2018-03-22 13:43:57 -0300

    Luis Integration bot

--- source/laico.bot/Controllers/MessagesController.cs
+++ source/laico.bot/Controllers/MessagesController.cs
@@ -1,8 +1,13 @@
-﻿using System.Net;
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using laico.bot.Dialogs;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Connector;
 
 namespace laico.bot
@@ -14,11 +19,60 @@ namespace laico.bot
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
+        //public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        //{
+        //    //var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+        //    //var attributes = new LuisModelAttribute(
+        //    //    ConfigurationManager.AppSettings["LuisId"],
+        //    //    ConfigurationManager.AppSettings["LuisSubscriptionKey"]);
+
+        //    //var service = new LuisService(attributes);
+
+        //    //switch (activity.Type)
+        //    //{
+        //    //    case ActivityTypes.Message:
+        //    //        await Conversation.SendAsync(activity, () => new LuisConversationDialog());
+        //    //        break;
+        //    //    case ActivityTypes.ConversationUpdate:
+        //    //        if(activity.MembersAdded.Any(o => o.Id == activity.Recipient.Id))
+        //    //        {
+        //    //            var reply = activity.CreateReply();
+        //    //            reply.Text = "Olá, sou a LAICO o seu assistente virtual político. \nFaça uma pergunta que terei o prazer em tentar responder.";
+        //    //            //await Conversation.SendAsync(activity, () => activity.pos)
+        //    //        }
+        //    //        break;
+        //    //}
+
+        //    //if (activity.Type == ActivityTypes.Message)
+        //    //{
+        //    //    //await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+        //    //    await Conversation.SendAsync(activity, () => new LuisConversationDialog());
+        //    //}
+        //    //else
+        //    //{
+        //    //    HandleSystemMessage(activity);
+        //    //}
+        //    //var response = Request.CreateResponse(HttpStatusCode.OK);
+        //    //return response;
+
+        //    if (activity.Type == ActivityTypes.Message)
+        //    {
+        //        await Conversation.SendAsync(activity, () => new LuisConversationDialog());
+        //    }
+        //    else
+        //    {
+        //        HandleSystemMessage(activity);
+        //    }
+        //    var response = Request.CreateResponse(HttpStatusCode.OK);
+        //    return response;
+        //}
+
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                await Conversation.SendAsync(activity, () => new ConversationLuisDialog());
             }
             else
             {
@@ -31,7 +85,7 @@ namespace laico.bot
         private Activity HandleSystemMessage(Activity message)
         {
             if (message.Type == ActivityTypes.DeleteUserData)
-            {
+            {                
                 // Implement user deletion here
                 // If we handle user deletion, return a real message
             }
@@ -53,7 +107,7 @@ namespace laico.bot
             else if (message.Type == ActivityTypes.Ping)
             {
             }
-
+            System.Diagnostics.Debug.WriteLine(message.Type.ToString());
             return null;
         }
     }
---
+++ source/laico.bot/Dialogs/ConversationLuis.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace laico.bot.Dialogs
+{
+    [Serializable]
+    //[LuisModel("b51c211f-7b5c-4e22-bc73-d86aee5eb60e", "cf39568e030e4e12b104539fa51df9c9")]
+    [LuisModel("5f8e53e5-2a84-4056-9ab1-cb043589e5b9", "cf39568e030e4e12b104539fa51df9c9")]
+    public class ConversationLuisDialog : LuisDialog<object>
+    {
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Desculpe, não consegui entender a sua frase **" + result.Query + "**");
+        }
+
+        [LuisIntent("localizacao")]
+        public async Task Localizacao(IDialogContext context, LuisResult result)
+        {
+            var feiras = result.Entities?.Select(e => e.Entity);
+
+            await context.PostAsync($"Você entrou na parte de localização {string.Join(",", feiras.ToArray())} ");
+        }
+
+        [LuisIntent("saudacao")]
+        public async Task Saudacao(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Olá eu sou um Bot, estou sempre aprendendo.");
+        }
+
+        [LuisIntent("sobre")]
+        public async Task Sobre(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Olá eu sou um Bot, estou sempre aprendendo.");
+        }
+
+        [LuisIntent("cumprimentos")]
+        public async Task Cumprimentos(IDialogContext context, LuisResult result)
+
+        {
+            await context.PostAsync("Olá, como vai você ?");
+        }        
+    }
+}
